f79b535dd24d3628181dcd25a1af624a11565b86
Davidyoon <davidyoon@koreainvestment.com>
2022-12-05 08:33:53 +0900

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by jiwon Yoon on 2022/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
